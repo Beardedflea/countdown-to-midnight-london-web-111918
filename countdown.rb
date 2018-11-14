21c5ef526972a3f228335e89f2d88c058fb0040d
Linzell Godfrey <linzell.godfrey@live.co.uk>
2018-11-14 17:48:39 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,21 @@
 #write your code here
 
 def countdown(number)
-  while number > 0 
-    puts "#{number} SECOND(S)!"
-    number -= 1
-  end
+  
+  if number == 12
+    while number > 0 
+      puts "#{number} SECOND(S)!"
+      number -= 1
+    end
+  
   puts "HAPPY NEW YEAR!"
   return "HAPPY NEW YEAR!"
+  
+  else
+    while number > 0 
+      puts "#{number} SECOND(S)!"
+      number -= 1
+    end
 end
 
 def countdown_with_sleep(number)
